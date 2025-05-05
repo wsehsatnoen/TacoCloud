d1284130656241a80f663282bd5cf17d7ac4e71d
Shane Darilek <wsehsatnoen@Shanes-MacBook-Pro.local>
2025-05-05 16:25:46 -0500

    Added processOrder PostMapping

--- src/main/java/me/wsehsatnoen/tacocloud/web/OrderController.java
+++ src/main/java/me/wsehsatnoen/tacocloud/web/OrderController.java
@@ -1,10 +1,13 @@
 package me.wsehsatnoen.tacocloud.web;
 
 import lombok.extern.slf4j.Slf4j;
+import me.wsehsatnoen.tacocloud.tacos.TacoOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -15,6 +18,13 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder tacoOrder, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", tacoOrder);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 
 }
