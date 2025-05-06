fccc6722d636a8015d90f1a43ceda79593fe62cf
swdar <swdarilek@gmail.com>
2025-05-05 23:09:14 -0500

    added validation to the controllers and views on the html

--- src/main/java/me/wsehsatnoen/tacocloud/web/DesignTacoController.java
+++ src/main/java/me/wsehsatnoen/tacocloud/web/DesignTacoController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+import jakarta.validation.Valid;
 import me.wsehsatnoen.tacocloud.tacos.Ingredient;
 import me.wsehsatnoen.tacocloud.tacos.Ingredient.Type;
 import me.wsehsatnoen.tacocloud.tacos.Taco;
 import me.wsehsatnoen.tacocloud.tacos.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
@@ -57,7 +59,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}" + taco);
 
--- src/main/java/me/wsehsatnoen/tacocloud/web/OrderController.java
+++ src/main/java/me/wsehsatnoen/tacocloud/web/OrderController.java
@@ -1,8 +1,10 @@
 package me.wsehsatnoen.tacocloud.web;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import me.wsehsatnoen.tacocloud.tacos.TacoOrder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +23,11 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder tacoOrder, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder tacoOrder, Errors errors, SessionStatus sessionStatus) {
+
+        if  (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", tacoOrder);
         sessionStatus.setComplete();
         return "redirect:/";
